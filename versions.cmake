7bc07da6a1d022cfebf4b3df9982c7ba2ea6c924
Ben Boeckel <mathstuf@gmail.com>
2014-04-24 12:20:10 -0400

    Fix some matviz names

--- versions.cmake
+++ versions.cmake
@@ -8,9 +8,15 @@
 #   ..
 # endif()
 
-add_revision(python
-  URL "http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz"
-  URL_MD5 "2cf641732ac23b18d139be077bd906cd")
+if (WIN32)
+  add_revision(python
+    URL "http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz"
+    URL_MD5 "2cf641732ac23b18d139be077bd906cd")
+else()
+  add_revision(python
+    URL "http://paraview.org/files/v3.98/dependencies/Python-2.7.2.tgz"
+    URL_MD5 "0ddfe265f1b3d0a8c2459f5bf66894c7")
+endif()
 
 add_revision(qt
   URL "http://download.qt-project.org/archive/qt/4.8/4.8.2/qt-everywhere-opensource-src-4.8.2.tar.gz"
