dc9361c23624c295fbfe73b3dced6a1a66b244a4
Ben Boeckel <ben.boeckel@kitware.com>
2014-04-25 15:12:41 -0400

    Attempt a fix at the OS X matviz bundle

--- projects/apple/matviz.bundle.cmake
+++ projects/apple/matviz.bundle.cmake
@@ -1,12 +1,13 @@
 include(matviz.bundle.common)
 
+include(paraview.bundle)
+
 set (CPACK_GENERATOR DragNDrop)
 include(CPack)
 
-# we only to paraview explicitly.
 install(CODE "
      file(INSTALL DESTINATION \"\${CMAKE_INSTALL_PREFIX}\" USE_SOURCE_PERMISSIONS TYPE DIRECTORY FILES
-          \"${install_location}/bin/matviz.app\")
+          \"${install_location}/Applications/matviz.app\")
 
      file(WRITE \"\${CMAKE_INSTALL_PREFIX}/matviz.app/Contents/Resources/qt.conf\"
                 \"\")
@@ -14,7 +15,20 @@ install(CODE "
        COMMAND ${CMAKE_CURRENT_LIST_DIR}/fixup_bundle.py
                \"\${CMAKE_INSTALL_PREFIX}/matviz.app\"
                \"${install_location}/lib\"
-               \"${SuperBuild_BINARY_DIR}/paraview/src/paraview-build/lib\"
+               \"${install_location}/plugins\")
+   "
+   COMPONENT superbuild)
+
+install(CODE "
+     file(INSTALL DESTINATION \"\${CMAKE_INSTALL_PREFIX}\" USE_SOURCE_PERMISSIONS TYPE DIRECTORY FILES
+          \"${install_location}/Applications/paraview.app\")
+
+     file(WRITE \"\${CMAKE_INSTALL_PREFIX}/paraview.app/Contents/Resources/qt.conf\"
+                \"\")
+     execute_process(
+       COMMAND ${CMAKE_CURRENT_LIST_DIR}/fixup_bundle.py
+               \"\${CMAKE_INSTALL_PREFIX}/paraview.app\"
+               \"${install_location}/lib\"
                \"${install_location}/plugins\")
    "
    COMPONENT superbuild)
