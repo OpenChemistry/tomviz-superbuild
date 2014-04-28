9fe695886e22006f94f786d3c4106d8bb0b51f1c
Ben Boeckel <mathstuf@gmail.com>
2014-04-28 14:00:07 -0400

    Remove ParaView from the DMG

--- projects/apple/matviz.bundle.cmake
+++ projects/apple/matviz.bundle.cmake
@@ -16,17 +16,3 @@ install(CODE "
                \"${install_location}/plugins\")
    "
    COMPONENT superbuild)
-
-install(CODE "
-     file(INSTALL DESTINATION \"\${CMAKE_INSTALL_PREFIX}\" USE_SOURCE_PERMISSIONS TYPE DIRECTORY FILES
-          \"${install_location}/Applications/paraview.app\")
-
-     file(WRITE \"\${CMAKE_INSTALL_PREFIX}/paraview.app/Contents/Resources/qt.conf\"
-                \"\")
-     execute_process(
-       COMMAND ${CMAKE_CURRENT_LIST_DIR}/fixup_bundle.py
-               \"\${CMAKE_INSTALL_PREFIX}/paraview.app\"
-               \"${install_location}/lib\"
-               \"${install_location}/plugins\")
-   "
-   COMPONENT superbuild)
