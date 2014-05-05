12e7969c1f44b0795d642ece1bf4d39b76a872b1
Ben Boeckel <ben.boeckel@kitware.com>
2014-05-05 14:11:46 -0400

    dashboard: set the project name

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,7 +4,7 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-#set(CTEST_PROJECT_NAME "MatViz")
+set(CTEST_PROJECT_NAME "MatViz")
 set(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
 
 set(CTEST_DROP_METHOD "http")
