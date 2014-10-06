c5cc1abcf1427d7ce50a154ce59121d81f8bef55
Ben Boeckel <ben.boeckel@kitware.com>
2014-10-06 14:41:31 -0400

    tomviz: handle DESTDIR properly

--- projects/apple/tomviz.bundle.cmake
+++ projects/apple/tomviz.bundle.cmake
@@ -4,26 +4,26 @@ set(CPACK_GENERATOR DragNDrop)
 include(CPack)
 
 install(CODE "
-     file(INSTALL DESTINATION \"\${CMAKE_INSTALL_PREFIX}\" USE_SOURCE_PERMISSIONS TYPE DIRECTORY FILES
+     file(INSTALL DESTINATION \"\ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}\" USE_SOURCE_PERMISSIONS TYPE DIRECTORY FILES
           \"${install_location}/Applications/tomviz.app\")
 
-     file(WRITE \"\${CMAKE_INSTALL_PREFIX}/tomviz.app/Contents/Resources/qt.conf\"
+     file(WRITE \"\ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/tomviz.app/Contents/Resources/qt.conf\"
                 \"\")
 
     # install all python modules/packages from paraview.
-    file(INSTALL DESTINATION \"\${CMAKE_INSTALL_PREFIX}/tomviz.app/Contents/Python\"
+    file(INSTALL DESTINATION \"\ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/tomviz.app/Contents/Python\"
          USE_SOURCE_PERMISSIONS TYPE DIRECTORY FILES
          \"${install_location}/Applications/paraview.app/Contents/Python/\")
 
     # install all other libraries from paraview (this is needed for the PythonD-*.dylib files
     # which don't get copied over by the fixup_bundle.py
-    file(INSTALL DESTINATION \"\${CMAKE_INSTALL_PREFIX}/tomviz.app/Contents/Libraries\"
+    file(INSTALL DESTINATION \"\ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/tomviz.app/Contents/Libraries\"
          USE_SOURCE_PERMISSIONS TYPE DIRECTORY FILES
          \"${install_location}/Applications/paraview.app/Contents/Libraries/\")
 
     execute_process(
        COMMAND ${CMAKE_CURRENT_LIST_DIR}/fixup_bundle.py
-               \"\${CMAKE_INSTALL_PREFIX}/tomviz.app\"
+               \"\ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/tomviz.app\"
                \"${install_location}/lib\"
                \"${install_location}/plugins\")
    "
