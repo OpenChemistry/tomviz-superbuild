b36fd43844609136677008670b2a261d1cb045b3
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2014-11-13 21:52:14 -0500

    Added some code to download/install example data files

--- projects/tomviz.cmake
+++ projects/tomviz.cmake
@@ -4,4 +4,5 @@ add_external_project(tomviz
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=ON
     -DParaView_DIR:PATH=${SuperBuild_BINARY_DIR}/paraview/src/paraview-build
+    -Dtomviz_data_DIR:PATH=${tomviz_data}
 )
