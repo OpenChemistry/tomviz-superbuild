58193af2cd0bb68edcd9d065c1b7325a53bf3859
Shawn Waldon <shawn.waldon@kitware.com>
2015-05-15 16:06:41 -0400

    Add scipy - should work on OSX and linux

--- projects/numpy.cmake
+++ projects/numpy.cmake
@@ -5,12 +5,36 @@ if (WIN32)
   set (_install_location "../../../install")
 endif()
 
+set(NUMPY_PROCESS_ENVIRONMENT)
+if(lapack_ENABLED)
+  if(NOT LAPACK_FOUND)
+    find_package(LAPACK REQUIRED)
+  endif()
+  list(APPEND NUMPY_PROCESS_ENVIRONMENT
+    MKL "None"
+    ATLAS "None"
+    BLAS "<INSTALL_DIR>/lib"
+    LAPACK "<INSTALL_DIR>/lib"
+  )
+endif()
+
+set (numpy_extra_depends)
+if (scipy_ENABLED)
+  set (numpy_extra_depends lapack)
+endif()
+
+# If any variables are set, we must have the PROCESS_ENVIRONMENT keyword
+if(NUMPY_PROCESS_ENVIRONMENT)
+  list(INSERT NUMPY_PROCESS_ENVIRONMENT 0 PROCESS_ENVIRONMENT)
+endif()
+
 add_external_project(numpy
-  DEPENDS python
+  DEPENDS python ${numpy_extra_depends}
   CONFIGURE_COMMAND ""
   INSTALL_COMMAND
     ${pv_python_executable} setup.py install --prefix=${_install_location}
   BUILD_IN_SOURCE 1
   BUILD_COMMAND
-    ${pv_python_executable} setup.py build --fcompiler=no
+    ${pv_python_executable} setup.py build --fcompiler=${CMAKE_Fortran_COMPILER}
+  ${NUMPY_PROCESS_ENVIRONMENT}
 )
---
+++ projects/win32/scipy.cmake
@@ -0,0 +1,2 @@
+add_external_dummy_project(scipy
+    DEPENDS python)
