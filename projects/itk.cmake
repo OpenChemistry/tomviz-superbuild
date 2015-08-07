bace66a54a5e51a1442f31fc11b853d848dc8254
Shawn Waldon <shawn.waldon@kitware.com>
2015-08-07 18:01:04 -0400

    ITK: superbuild with itk on windows

---
+++ projects/itk.cmake
@@ -0,0 +1,8 @@
+add_external_project(itk
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND
+    "${CMAKE_COMMAND}"
+      "-Dinstall_dir:PATH=<INSTALL_DIR>"
+      -P "${CMAKE_CURRENT_LIST_DIR}/itk.install.cmake")
