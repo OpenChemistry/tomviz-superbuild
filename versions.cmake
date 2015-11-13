8fab67b16274a1be1c468edd9389fced0da4f68e
Shawn Waldon <shawn.waldon@kitware.com>
2015-11-13 13:14:28 -0500

    Add ITK binary tarball for linux

--- versions.cmake
+++ versions.cmake
@@ -139,4 +139,8 @@ elseif(APPLE)
   add_revision(itk
     URL "http://www.tomviz.org/files/itk-4.8.0-macosx10.8.tar.gz"
     URL_MD5 "904a0e8c7ebe986deddceb4984bf455d")
+elseif(UNIX)
+  add_revision(itk
+    URL "http://www.tomviz.org/files/itk-4.8.1-linux-64bit.tar.gz"
+    URL_MD5 "e0ceef7aee666704036c7ecc1f532015")
 endif()
