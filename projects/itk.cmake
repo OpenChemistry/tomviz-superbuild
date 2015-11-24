79c5f7b1c68b889fc143b50e0c3748fc244867a7
Shawn Waldon <shawn.waldon@kitware.com>
2015-11-24 10:42:52 -0500

    fftw has different target name on windows since it is one blob

--- projects/itk.cmake
+++ projects/itk.cmake
@@ -1,5 +1,9 @@
+set(fftw_targets fftw3double fftw3float fftw3long)
+if (WIN32)
+  set(fftw_targets fftw)
+endif()
 add_external_project(itk
-  DEPENDS fftw
+  DEPENDS ${fftw_targets}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
