c1d503c31133db5253bde4ac554669e90dc65872
Chris Harris <chris.harris@kitware.com>
2017-03-16 15:42:52 -0400

    Add extra cmake args for Python

--- projects/python.cmake
+++ projects/python.cmake
@@ -7,3 +7,9 @@ add_external_project_or_use_system(python
   PROCESS_ENVIRONMENT CFLAGS -I${CMAKE_OSX_SYSROOT}/usr/include
   )
 set (pv_python_executable "${install_location}/bin/python3" CACHE INTERNAL "" FORCE)
+
+add_extra_cmake_args(
+  -DPYTHON_EXECUTABLE:FILEPATH=<INSTALL_DIR>/bin/python3
+  -DPYTHON_INCLUDE_DIR:PATH=<INSTALL_DIR>/include/python3.6m
+  -DPYTHON_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/libpython3.6m.dylib
+  -DVTK_PYTHON_VERSION:STRING=3)
