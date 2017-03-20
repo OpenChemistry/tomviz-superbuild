a87ae1cf04fe835354af514f44de9f88b3f7f3ce
Chris Harris <chris.harris@kitware.com>
2017-03-20 10:01:50 -0400

    Use CFLAGS to set include path

--- projects/python.cmake
+++ projects/python.cmake
@@ -3,7 +3,7 @@ add_external_project_or_use_system(python
   CONFIGURE_COMMAND <SOURCE_DIR>/configure
                     --prefix=<INSTALL_DIR>
                     --enable-unicode=ucs4
-                    --enable-shared
+                    --enable-shared CFLAGS=-I${CMAKE_OSX_SYSROOT}/usr/include
   PROCESS_ENVIRONMENT CFLAGS -I${CMAKE_OSX_SYSROOT}/usr/include
   )
 set (pv_python_executable "${install_location}/bin/python3" CACHE INTERNAL "" FORCE)
