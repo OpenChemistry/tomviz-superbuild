0260d2bb746e3f9d3091b84cd419f78f14a1170c
Chris Harris <chris.harris@kitware.com>
2017-03-21 10:20:41 -0400

    Use CMAKE_SHARED_LIBRARY_SUFFIX for python lib

--- projects/python.cmake
+++ projects/python.cmake
@@ -10,5 +10,5 @@ set (pv_python_executable "${install_location}/bin/python3" CACHE INTERNAL "" FO
 add_extra_cmake_args(
   -DPYTHON_EXECUTABLE:FILEPATH=<INSTALL_DIR>/bin/python3
   -DPYTHON_INCLUDE_DIR:PATH=<INSTALL_DIR>/include/python3.6m
-  -DPYTHON_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/libpython3.6m.dylib
+  -DPYTHON_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/libpython3.6m${CMAKE_SHARED_LIBRARY_SUFFIX}
   -DVTK_PYTHON_VERSION:STRING=3)
