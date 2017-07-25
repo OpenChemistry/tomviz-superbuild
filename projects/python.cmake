397000869fd8484e9f9958ccdccd08f28e1cf577
Shawn Waldon <shawn.waldon@kitware.com>
2017-07-25 10:41:31 -0400

    Move macOS-specific code inside if(APPLE) block

--- projects/python.cmake
+++ projects/python.cmake
@@ -1,10 +1,12 @@
-set(_proc_env "MACOSX_DEPLOYMENT_TARGET" "10.9")
+set(_proc_env)
+
+if (APPLE)
+  list(APPEND _proc_env "MACOSX_DEPLOYMENT_TARGET" "10.9")
 
 # Horrible hack to force the configure step not to find getentropy and clock_gettime.
 # If we build on a version that has these defined ( even as weak symbols ) they will
 # be used and then we will get run time link errors when running on versions that
 # don't actually have them defined ( such as 10.11 )
-if (APPLE)
   list(APPEND _proc_env "ac_cv_func_getentropy" "no" "ac_cv_func_clock_gettime" "no")
 endif()
 
