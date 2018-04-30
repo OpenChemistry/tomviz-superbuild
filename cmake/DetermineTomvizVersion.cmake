049b5091170c186f15d1285ecb1b8dbfccee02b8
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2018-04-29 22:55:32 -0400

    Bump the version - force a build

--- cmake/DetermineTomvizVersion.cmake
+++ cmake/DetermineTomvizVersion.cmake
@@ -7,7 +7,7 @@
 #   tomviz_version
 #   tomviz_version_long
 
-set(hardcoded_tomviz_version "1.2.1")
+set(hardcoded_tomviz_version "1.3.0-rc1")
 
 function(_set_version_vars versiontext)
   string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)[-]*(.*)" version_matches "${versiontext}")
