9504326ab2eb5da9ba0ec8c93954cc5775ff3e92
Patrick Avery <patrick.avery@kitware.com>
2019-05-06 17:39:28 -0400

    Use python 3.7 for numpy and scipy
    The numpy and scipy versions will be updated next...
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- projects/apple/numpy.cmake
+++ projects/apple/numpy.cmake
@@ -1,3 +1,3 @@
-set(numpy_platform_string "cp36-cp36m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64")
+set(numpy_platform_string "cp37-cp37m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64")
 
 include(numpy.common)
