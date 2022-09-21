054d307bcf47cdcda437c630b904f559949250d8
Chris Harris <chris.harris@kitware.com>
2022-09-20 22:18:15 -0400

    Set CMAKE_INSTALL_LIBDIR to lib
    Otherwise on centos libraries will in installed into lib64

--- projects/tomviz.cmake
+++ projects/tomviz.cmake
@@ -36,6 +36,7 @@ add_external_project(tomviz
   DEPENDS paraview qt
 
   CMAKE_ARGS
+    -DCMAKE_INSTALL_LIBDIR:STRING=lib
     -DBUILD_SHARED_LIBS:BOOL=ON
     -DParaView_DIR:PATH=${SuperBuild_BINARY_DIR}/paraview/src/paraview-build
     -Dtomviz_data_DIR:PATH=${tomviz_data}
