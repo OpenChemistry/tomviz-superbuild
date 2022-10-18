eed971697ac61ec7eca2067ae248580149caaf8c
Chris Harris <chris.harris@kitware.com>
2022-10-18 08:51:58 -0400

    Upgrade pip before installing numpy
    The older version of pip doesn't know about the newer versions of
    MacOS so rejects our wheel.

--- projects/apple/numpy.cmake
+++ projects/apple/numpy.cmake
@@ -1,3 +1,11 @@
 set(numpy_platform_string "cp37-cp37m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64")
 
-include(numpy.common)
+add_external_project(numpy
+  DEPENDS python
+  CONFIGURE_COMMAND ""
+  INSTALL_COMMAND
+  ${python_pip_executable} install -U pip
+  COMMAND ${python_pip_executable} install "numpy-1.16.3-${numpy_platform_string}.whl"
+  BUILD_IN_SOURCE 1
+  BUILD_COMMAND ""
+)
