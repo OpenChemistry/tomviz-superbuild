e2203b1276acd57a23d980eb0dd81c25f2ca2b03
Chris Harris <chris.harris@kitware.com>
2022-11-08 14:40:48 -0500

    Push ParaView forward

--- versions.cmake
+++ versions.cmake
@@ -89,7 +89,7 @@ if(USE_PARAVIEW_MASTER)
   set(_paraview_revision "master")
 else()
   # Test the revision locally before proposing a move
-  set(_paraview_revision "6d9192059fe998cf7004dd85638ccf33f89e6976")
+  set(_paraview_revision "6f7d870595bf68b02dcf63c1a56adf6eb53dd2f1")
 endif()
 # Locally patched ParaView repo, or main repo
 #set(_paraview_repo "https://gitlab.kitware.com/paraview/paraview.git")
